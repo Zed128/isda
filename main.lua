eb54821716518211716c9a6c7fe61d343c25b0d2
Zed128 <118735074+Zed128@users.noreply.github.com>
2025-01-11 22:50:51 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -15,6 +15,8 @@ function getRodToolFromBackpack(): Instance
 	for i,v in pairs(LocalPlayer.Backpack:GetChildren()) do
 		if v:FindFirstChild("events") and v:FindFirstChild("events"):FindFirstChild("cast") then
 			return v
+		else
+			return nil
 		end
 	end
 	return nil
