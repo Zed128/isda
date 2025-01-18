56928ff8babf03133f3fcd96d9a5713f2df999eb
Zed128 <118735074+Zed128@users.noreply.github.com>
2025-01-18 20:14:40 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -165,12 +165,12 @@ task.spawn(function()
 
 		GuiService.SelectedObject = Button
 		print("selected object: ", GuiService.SelectedObject)
-		--keypress(Enum.KeyCode.Return)
-		--keyrelease(Enum.KeyCode.Return)
+		keypress(Enum.KeyCode.Return)
+		keyrelease(Enum.KeyCode.Return)
 
-		--GuiService.AutoSelectGuiEnabled = true
-		--GuiService.GuiNavigationEnabled = false
-		--GuiService.SelectedObject = nil
+		GuiService.AutoSelectGuiEnabled = true
+		GuiService.GuiNavigationEnabled = false
+		GuiService.SelectedObject = nil
 	end
 	LocalPlayer.PlayerGui.ChildAdded:Connect(function(child)
 		if child.Name == "shakeui" and child:IsA("ScreenGui") then
